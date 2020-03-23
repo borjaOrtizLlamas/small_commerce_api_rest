c94616eab9cf549f5ed46ae1775807be2252d680
borja <borja.ortizllamas1@gmail.com>
2020-03-23 01:13:58 +0100

    añadiendo testing JUnit

---
+++ src/test/java/com/borja/MainTest.java
@@ -0,0 +1,19 @@
+package com.borja;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+		 
+	     
+	  @Before
+	  public void setUp() {
+	  }
+	     
+	  @Test
+	  public void testGetSalarioBruto() {
+		  assertTrue(true);
+	  }
+}
